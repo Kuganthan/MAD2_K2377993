a66e6f7e472777849d1817eed87c40c05b3a500e
Kuganthan <152803959+Kuganthan@users.noreply.github.com>
2024-02-16 23:33:16 +0530

    Delete core directory

--- core/error/error.dart
+++
@@ -1,8 +0,0 @@
-library error;
-
-export 'failures.dart';
-export 'exceptions.dart';
-export 'firebase/google.dart';
-export 'firebase/login.dart';
-export 'firebase/signup.dart';
-export 'functions.dart';
